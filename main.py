900204dd9f957d39ff6d1b4cbb5b0d4a3185f873
dip00-bb <dipparthochondo@gmail.com>
2026-08-22 15:42:40 +0600

    create a welcome canvas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 from pathlib import Path
 from PIL import Image, ImageTk
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -10,17 +12,42 @@ class App(ctk.CTk):
         screen_height=self.winfo_screenheight()
 
         self.geometry(f"{screen_width}x{screen_height}")
-        self.minsize(screen_width,screen_height)
+        self.resizable(False,False)
 
         # title
         self.title("Merit Garden Girls School And College Management")
+
         # icon
         base_dir = Path(__file__).resolve().parent
         icon_path = base_dir / "src" / "school_management_app" / "assets" / "merit_icon.png"
 
         icon_image = Image.open(icon_path)
         self.icon = ImageTk.PhotoImage(icon_image)
+        # self.wm_iconphoto(True, self.icon)
         self.iconphoto(True, self.icon)
+
+        welcome_canvas=ctk.CTkCanvas(self,
+                                     height=screen_height,
+                                     width=screen_width,
+                                     bg="black",
+                                     highlightthickness=0
+                                     )
+
+        welcome_canvas.pack()
+        welcome_canvas.create_image(
+      0,
+            0,
+            image=self.icon,
+            anchor="center",
+
+        )
+        welcome_canvas.create_text(
+            screen_width/2,screen_height/2,
+            text="MERIT GARDEN GIRLS SCHOOL AND COLLEGE",
+            font=("Arial",50,"bold"),
+            fill="sky blue"
+        )
+
 
 if __name__=="__main__":
     app=App()
