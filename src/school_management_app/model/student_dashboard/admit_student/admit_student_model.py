0e47bc6d9085a69dcd1555f10201dcc2fd6da4f5
dip00-bb <dipparthochondo@gmail.com>
2026-09-19 17:00:13 +0600

    remove gender validation and add present, permanet address and school name validator

--- src/school_management_app/model/student_dashboard/admit_student/admit_student_model.py
+++ src/school_management_app/model/student_dashboard/admit_student/admit_student_model.py
@@ -1,4 +1,4 @@
-from ....utilitis import check_valid_name,is_valid_us_date,validate_bd_number,validate_gender,validate_email
+from ....utilitis import check_valid_name,is_valid_us_date,validate_bd_number,validate_email
 class AdmitStudentModel:
 
     def __init__(
@@ -37,7 +37,6 @@ class AdmitStudentModel:
         # check_valid_name(mother_name,"Mother Name",3,15)
         # check_valid_name(father_name,"Father Name",3,15)        
         # is_valid_us_date(date_of_birth)
-        validate_gender(gender)
         # validate_bd_number(phone_number)
         # validate_bd_number(whats_app_number)
         # validate_email(email)
